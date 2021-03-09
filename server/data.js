b405cf4e60afbc94c34cd1ad4ff28d68a40942d5
HeyAero <aeroyts@icloud.com>
2021-03-09 15:21:02 +0000

    add fake google website data

---
+++ server/data.js
@@ -0,0 +1,129 @@
+// Create base file searches
+// What to store:
+// 1. ID of stores website
+// 2. Title of website
+// 3. URL of page
+// 4. search keywords
+
+const searches = [
+  {
+    id: 1,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+  {
+    id: 2,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+  {
+    id: 3,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+  {
+    id: 4,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+  {
+    id: 5,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+  {
+    id: 6,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+  {
+    id: 7,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+  {
+    id: 8,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+  {
+    id: 9,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+  {
+    id: 10,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+  {
+    id: 11,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+  {
+    id: 12,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+  {
+    id: 13,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+  {
+    id: 14,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+  {
+    id: 15,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+  {
+    id: 16,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+  {
+    id: 17,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+  {
+    id: 18,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+  {
+    id: 19,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+  {
+    id: 20,
+    title: 'Football - BBC Sport',
+    url: 'https://www.bbc.co.uk/sport/football',
+    keywords: ['football', 'sports', 'foot', 'ball', 'scores', 'league'],
+  },
+];
